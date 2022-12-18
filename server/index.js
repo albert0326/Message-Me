c29f06e21651c021db09f95761f3025b3180c199
panimaya-gembrill <psalethrajan@gembrill.com>
2022-12-18 12:18:03 +0530

    bug fix

--- server/index.js
+++ server/index.js
@@ -37,6 +37,7 @@ const server = app.listen(process.env.PORT, () =>
 const io = socket(server, {
   cors: {
     origin: "https://message-me-ap.onrender.com",
+    methods: ["GET", "POST"],
     credentials: true,
   },
 });
